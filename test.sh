0789c7ea904cc26bc5aefc747bd5110a59108155
Kim, Joo-Ho <teamsprint@gmail.com>
2019-12-12 00:23:28 +0900

    add search id features

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #./preptool -s /tmp/s5k_1.csv
-./preptool -q "select * from test.t" -i test_ids
+#./preptool -q "select * from test.t" -i test_ids
+#./preptool --search-wds "s5k_ci" | head -1
+./preptool --search-connection "MySQL" | head -1
 
 #eof
