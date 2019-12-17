15a4a0aca9a36c103cff75ec5a8f6087d8ec6e2d
Kim, Joo-Ho <teamsprint@gmail.com>
2019-12-17 19:36:02 +0900

    add export dataflow feature

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#./preptool -s /tmp/s5k_1.csv
-#./preptool -q "select * from test.t" -i test_ids
-#./preptool --search-wds "s5k_ci" | head -1
+./preptool -s ~/data/s5k_1.csv
+./preptool -q "select * from test.t" --search-connection "MySQL" -i "DB test dataset"
+./preptool --search-wds "anonymous" | head -1
 ./preptool --search-connection "MySQL" | head -1
 
 #eof
