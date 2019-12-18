9268571421223122e71eea67adfef7917136d7cd
Kim, Joo-Ho <teamsprint@gmail.com>
2019-12-18 22:04:20 +0900

    implemented dataset swap

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-./preptool -s ~/data/s5k_1.csv
-./preptool -q "select * from test.t" --search-connection "MySQL" -i "DB test dataset"
-./preptool --search-wds "anonymous" | head -1
-./preptool --search-connection "MySQL" | head -1
-./preptool --search-dataflow "SKT"
+./preptool -f ~/data/s5k_1.csv
+./preptool -f ~/data/s5k_1.csv -n "Sales data 5000 rows (1/4)"
+./preptool -q "select * from test.t" -c "MySQL" -n "DB test dataset"
+./preptool -F multi -o sales_2011_02 -n sales_2011_3
 
 #eof
